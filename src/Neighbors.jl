5284010c9aa35724b9d482b87903b7f2c7f1c503
Danielle Raine <danielle.w.raine@outlook.com>
2024-08-16 23:23:02 -0500

    Create fit and predict for KNNClassifier
    Predict only works on one at a time and just shows the closest points
    so far. But it works!!!
    Demo was also updated as I tested it.

--- src/Neighbors.jl
+++ src/Neighbors.jl
@@ -1,10 +1,28 @@
 module Neighbors
 
 export KNNClassifier
+export fit!, predict
 
-struct KNNClassifier
+mutable struct KNNClassifier
         k::Int
         distance_function::Function
+        data
+        labels
+        KNNClassifier(k, distance_function) = new(k, distance_function, nothing, nothing)
+end
+
+function fit!(model, X, y)
+        # if size(X, 1) != size(y, 1) || length(size(y))  > 1
+        #         error("X and y must have the same number of records, and y must only have 1 dimension.")
+        # end
+        model.data = Array(X)
+        model.labels = Array(deepcopy(y))
+        return model
+end
+
+function predict(model::KNNClassifier, X)
+        distances = map(a -> model.distance_function(a, X), eachrow(model.data))
+        return sort([distances model.labels], dims=1)[1:model.k, :]
 end
 
 end
